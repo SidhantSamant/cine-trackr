f1da2898f9f589d49bb21580a905a8c345e82cfe
Sidhant Samant <sidhant775@gmail.com>
2026-06-14 15:16:02 +0530

    react compiler enabled

--- app.config.ts
+++ app.config.ts
@@ -61,6 +61,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     experiments: {
         typedRoutes: true,
         tsconfigPaths: true,
+        reactCompiler: true,
     },
     extra: {
         router: {},
